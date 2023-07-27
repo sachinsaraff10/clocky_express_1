0ddd57004daae049d2a1e51182189e7bcc52ce50
sachinsaraff10 <109337905+sachinsaraff10@users.noreply.github.com>
2023-07-27 15:47:04 +0530

    Latest update

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, ipcMain} = require('electron');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+
+    webPreferences: {
+      nodeIntegration: true, // Allow Node.js integration in the renderer process (use it only if you trust the content of your HTML file)
+    },
+  });
+
+  mainWindow.loadFile('index.html'); // Load the HTML file for the renderer process
+}
+
+app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
