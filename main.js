cf7407d3d3309e7c108f69d0f900bf739885a543
sachinsaraff10 <109337905+sachinsaraff10@users.noreply.github.com>
2023-07-28 01:57:15 +0530

    Local server set-up

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, ipcMain} = require('electron');
-
+const http = require('http');
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -9,6 +9,7 @@ function createMainWindow() {
       nodeIntegration: true, // Allow Node.js integration in the renderer process (use it only if you trust the content of your HTML file)
     },
   });
+const locserv1=http.createServer((res,req)=>{});
 
   mainWindow.loadFile('index.html'); // Load the HTML file for the renderer process
 }
@@ -29,5 +30,5 @@ app.on('activate', () => {
 
 ipcMain.on('url-added',(event,url)=>{
     urls.push(url);
-    
+
 })
