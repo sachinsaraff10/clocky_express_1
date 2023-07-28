bd142fe825b54f27d319ce248e86b0113d863030
sachinsaraff10 <109337905+sachinsaraff10@users.noreply.github.com>
2023-07-29 01:58:17 +0530

    added event listeners for active tabs and for adding a new tab

--- background.js
+++ background.js
@@ -1,8 +1,27 @@
 chrome.extension.getBackgroundPage();
+let urls=[];
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'monitorURL') {
       // Get the monitored URL from the message and add it to your monitoring list
       const monitoredURL = message.url;
+      urls.push(monitoredURL);
+      chrome.tabs.query({active:true,currentWindow:true},(tabs)=>{
+            for (let i=0;i<tabs.length;i++){
+                const releurl=tabs[i].url
+                if (releurl.includes(monitoredURL)){
+                    chrome.windows.create({
+                        url: 'timer.html',
+            type: 'popup',
+            width: 400,
+            height: 200,
+            left: screen.availWidth - 420, // Adjust the position to the bottom right
+            top: screen.availHeight - 220,
+                    })
+                }
+            }
+
+
+      })  
       // Perform your logic to monitor the URL and trigger the timer if needed
       // For example, you can store the monitoredURL in an array and check it against the active tabs using the chrome.tabs API
       // If the monitoredURL matches an active tab URL, open the timer page
@@ -23,8 +42,24 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   });
   
   // Listen for messages from content scripts or other parts of the extension
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // Handle the incoming messages
-    // You can distinguish different actions using the message.action property
-  });
+//   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     // Handle the incoming messages
+//     // You can distinguish different actions using the message.action property
+//   });
   
+  chrome.tabs.onUpdated.addListener((tabId,changeInfo,tab)=>{
+    if (changeInfo.status="complete"){
+        const taburl=tab.url;
+        for (let i=0;i<urls.length;i++)
+        {if (taburl.includes(urls[i])){
+            chrome.windows.create({
+                url: 'timer.html',
+    type: 'popup',
+    width: 400,
+    height: 200,
+    left: screen.availWidth - 420, // Adjust the position to the bottom right
+    top: screen.availHeight - 220,
+            })
+        }}
+    }
+  })
