37f27bb0d58372aa14e62d9565bffe82e1871429
sachinsaraff10 <109337905+sachinsaraff10@users.noreply.github.com>
2023-07-28 22:55:11 +0530

    Added background.js

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'monitorURL') {
+      // Get the monitored URL from the message and add it to your monitoring list
+      const monitoredURL = message.url;
+      // Perform your logic to monitor the URL and trigger the timer if needed
+      // For example, you can store the monitoredURL in an array and check it against the active tabs using the chrome.tabs API
+      // If the monitoredURL matches an active tab URL, open the timer page
+      // Example:
+      // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      //   const activeURL = tabs[0].url;
+      //   if (activeURL.includes(monitoredURL)) {
+      //     // Open the timer page using chrome.tabs.create()
+      //   }
+      // });
+    }
+  });
+  
+  // Other background script logic and tasks...
