f411204b7981d2fed1063996a3bdd85e0a82f6f0
sachinsaraff10 <109337905+sachinsaraff10@users.noreply.github.com>
2023-08-14 10:59:35 +0530

    fixed small typo wrt storing running url after clearing it and also adding curly brackets around set function arguments inshaAllah it makes a difference

--- background.js
+++ background.js
@@ -391,7 +391,8 @@ chrome.tabs.onUpdated.addListener((tabId,changeInfo,tab)=>{
                   object:running_timer},(response)=>{
                     let pausedtimer=response.object;
                     timer_overwrite[running_url[0]]=pausedtimer;
-                    chrome.storage.local.set(overwritten:timer_overwrite)
+                    running_url=[];
+                    chrome.storage.local.set({overwritten:timer_overwrite,running:running_url})
                   });
         
                 }
